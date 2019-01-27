2b416dfa15905e9193cec2ac765e4948823b8a50
Veeti Haapsamo <veeti.haapsamo@gmail.com>
2019-01-26 17:26:02 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::error::Error;
+
+use lambda_runtime::{error::HandlerError, lambda, Context};
+use log::{self, error};
+use serde_derive::{Deserialize, Serialize};
+use simple_error::bail;
+use simple_logger;
+
+#[derive(Deserialize)]
+struct CustomEvent {}
+
+#[derive(Serialize)]
+struct CustomOutput {
+    message: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    simple_logger::init_with_level(log::Level::Debug)?;
+    lambda!(my_handler);
+
+    Ok(())
+}
+
+fn my_handler(e: CustomEvent, c: Context) -> Result<CustomOutput, HandlerError> {
+    Ok(CustomOutput {
+        message: format!("Hello, Rust!"),
+    })
+}
